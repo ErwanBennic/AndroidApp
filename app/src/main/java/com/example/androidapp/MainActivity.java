bae75018483cd13968e569e388ad3376178a8486
Erwan Bennic <erwanb04@gmail.com>
2020-04-14 11:37:14 +0200

    Update MainActivity.java
    Removed action bar + added comments

--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,12 +8,18 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 2000; // Splash duration
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // Change intent after splash timeout
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
