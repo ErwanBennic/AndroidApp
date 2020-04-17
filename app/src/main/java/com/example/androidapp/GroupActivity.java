ef9cfa155c4ddc7b886ab80e1a726500ad100c96
Erwan Bennic <erwanb04@gmail.com>
2020-04-17 10:48:08 +0200

    Group activity + UI + action bar

--- app/src/main/java/com/example/androidapp/GroupActivity.java
+++ app/src/main/java/com/example/androidapp/GroupActivity.java
@@ -2,19 +2,15 @@ package com.example.androidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
 
-public class HomeActivity extends AppCompatActivity {
+public class GroupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        setTitle("EPSI");
+        setContentView(R.layout.activity_group);
+        setTitle("Infos");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -22,10 +18,5 @@ public class HomeActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         finish();
         return true;
-    }
-
-    public void goToGroups(View view) {
-        Intent intent = new Intent(HomeActivity.this, GroupActivity.class);
-        startActivity(intent);
     }
 }
